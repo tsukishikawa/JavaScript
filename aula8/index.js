a54fedb468727fb4c0c1b6372652081b0b46abdd
Mariana Ishikawa <mariishikawamota@hotmail.com>
2023-06-09 18:35:16 -0300

    strings

---
+++ aula8/index.js
@@ -0,0 +1,19 @@
+/*  strings
+// usar barra invertida para colocar aspas duplas dentro de uma string. 
+
+let umaString = '\"um texto"\'; 
+
+console.log(umaString);
+
+*Concatenação 
+ let umaString = "Um texto";
+(3 maneiras)
+ console.log(umaString.concat(' em um lindo dia.'));
+ console.log(umaString + 'em um lindo dia.');
+ console.log('${umaString} em um lindo dia.');
+
+ *Substituição de palavras
+
+ let umaString = "Um texto";
+
+ console.log(umaString.replace('Um', 'Outro'));
