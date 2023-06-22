220725e70b2abf42ad2bf165af05fdb2233941ff
Mariana Ishikawa <mariishikawamota@hotmail.com>
2023-06-22 11:48:35 -0300

    exercicio.math

--- aula11/script.js
+++ aula11/script.js
@@ -1,4 +1,4 @@
-const numero = prompt('Digite um número:');
+const numero = Number(prompt('Digite um número:'));
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
