c654c7f71a4b2ed91f7235f9cc24ace1011f2986
Mariana Ishikawa <mariishikawamota@hotmail.com>
2023-06-22 11:34:07 -0300

    exercicio.math

---
+++ aula10/script.js
@@ -0,0 +1,6 @@
+const numero = prompt('Digite um número:');
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p>Seu número - 2 é ${numero - 2}</p>`; 
