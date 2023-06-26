c4145c476d117e767f896d2b5b5e13a511626abe
Mariana Ishikawa <mariishikawamota@hotmail.com>
2023-06-26 11:13:41 -0300

    arrays

---
+++ aula12/script.js
@@ -0,0 +1,10 @@
+// arrays with strings
+const nome = 'Luiz Otávio';
+
+const alunos = ['Luiz', 'Maria', 'João'];
+alunos [3] = 'Luiza'; // utilizando unshift adiciona os elementos no começo do array
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[3]);
+// para remover utilize alunos.pop()
+
